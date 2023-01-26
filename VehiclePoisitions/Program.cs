f40294fa54841ef5e86a0242e2a0730b9600a66e
morris@mfundopedia.co.za <morris@mfundopedia.co.za>
2023-01-26 11:44:16 +0200

    set stop watch

--- VehiclePoisitions/Program.cs
+++ VehiclePoisitions/Program.cs
@@ -3,7 +3,7 @@ using System;
 using System.Text.Json;
 using VehiclePoisitions;
 using VehiclePoisitions.Repositories;
-
+using System.Diagnostics;
 namespace VehiclePosition
 {
     internal class Program
@@ -11,6 +11,11 @@ namespace VehiclePosition
         private const int VEHICLE_COUNT = 2000000;
         private static void Main(string[] args)
         {
+            var timer = new Stopwatch();
+           
+            timer.Start();
+
+            Console.WriteLine("Time start...");
             VehicleRepository vehicleRepository = new VehicleRepository(VEHICLE_COUNT);
             var vehicleList = vehicleRepository.GetVehiclesList();
             var vehiclePositionList = vehicleRepository.GetAllVehiclePositions();
@@ -22,7 +27,11 @@ namespace VehiclePosition
                  Console.WriteLine("Possition (Longitude: {0} Lattitude: {1} ) Nearest Coords: {2}",position.Longitude,position.Latitude, nearestPosition.First());
               
             }
-          
+            timer.Stop();
+
+            TimeSpan timeTaken = timer.Elapsed;
+
+            Console.WriteLine("Time Taken:{0:c}",timeTaken);
         }
     }
 }
